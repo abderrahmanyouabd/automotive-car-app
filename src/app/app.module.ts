30bfe037c3fd04dfd62a4716b4c2e49d2b385eac
Abderrahman Youabd <youabd50@gmail.com>
2024-07-10 00:09:35 +0200

    feat: landing page comp added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,21 @@ import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import LandingPageComponent from "./components/landing-page/landing-page.component";
+// import { LandingPageComponent } from './components/landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    // LandingPageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     StoreModule.forRoot({}, {}),
     EffectsModule.forRoot([]),
-    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() })
+    StoreDevtoolsModule.instrument({maxAge: 25, logOnly: !isDevMode()}),
+    LandingPageComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/landing-page/landing-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landing-page',
+  templateUrl: './landing-page.component.html',
+  standalone: true,
+  styleUrl: './landing-page.component.scss'
+})
+export default class LandingPageComponent {
+
+}
