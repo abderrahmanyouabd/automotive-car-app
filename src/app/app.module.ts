e316d8d9ab93dc0ebea0a9ce6ab8dbf085f02430
Abderrahman Youabd <youabd50@gmail.com>
2024-07-10 20:06:11 +0200

    feat: landing page design improved and main type created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {TopNavigationComponent} from "./components";
+import {LandingPageComponent} from "./components";
+
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import {TopNavigationComponent} from "./components";
-import {LandingPageComponent} from "./components";
 import { AvatarModule } from 'primeng/avatar';
 import {CardModule} from "primeng/card";
+import { DataViewModule } from 'primeng/dataview';
+import {ButtonModule} from "primeng/button";
+import {RatingModule} from "primeng/rating";
+import {FormsModule} from "@angular/forms";
+import {CommonModule} from "@angular/common";
+import {TagModule} from "primeng/tag";
 
 @NgModule({
   declarations: [
@@ -24,7 +31,13 @@ import {CardModule} from "primeng/card";
     EffectsModule.forRoot([]),
     StoreDevtoolsModule.instrument({maxAge: 25, logOnly: !isDevMode()}),
     AvatarModule,
-    CardModule
+    CardModule,
+    DataViewModule,
+    ButtonModule,
+    RatingModule,
+    FormsModule,
+    CommonModule,
+    TagModule
   ],
   providers: [],
   exports: [
--- src/app/components/landing-page/landing-page.component.ts
+++ src/app/components/landing-page/landing-page.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Observable, of} from "rxjs";
+
+type Car =  {
+  id?: number,
+  brand?: string,
+  modell?: string,
+  price?: number,
+  rating?: number,
+  image?: string,
+  InStockStatus?: 'IN STOCK' | 'OUT OF STOCK'
+}
+
+type Cars = Array<Car>;
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
-export default class LandingPageComponent {
+export default class LandingPageComponent{
 
+  public automotives$: Observable<Cars> = this.getCars();
+
+
+  private getCars(): Observable<Cars> {
+    return of([
+      {
+        id: 1, brand: 'Bmw', model: 'rgx2009', price: 4000, rating: 4.3, InStockStatus: 'IN STOCK'
+      },
+      {
+        id: 2, brand: 'Hyundai', model: 'pml5980', price: 5000, rating: 4.5, InStockStatus: 'IN STOCK'
+      },
+      {
+        id: 3, brand: 'Peugeo', model: 'rgx2009', price: 4700, rating: 4.7, InStockStatus: 'IN STOCK'
+      },
+      {
+        id: 4, brand: 'Bmw', model: 'rgx2009', price: 3700, rating: 3.5, InStockStatus: 'IN STOCK'
+      }
+
+    ])
+  }
 }
