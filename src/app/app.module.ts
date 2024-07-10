e2cdf7d43ec7baa59dee7fa3ae474391259098f8
Abderrahman Youabd <youabd50@gmail.com>
2024-07-10 02:04:21 +0200

    feat: adding some styling to the landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import LandingPageComponent from "./components/landing-page/landing-page.component";
-// import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import {TopNavigationComponent} from "./components";
+import {LandingPageComponent} from "./components";
+import { AvatarModule } from 'primeng/avatar';
 
 @NgModule({
   declarations: [
     AppComponent,
-    // LandingPageComponent
+    TopNavigationComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
@@ -20,9 +22,12 @@ import LandingPageComponent from "./components/landing-page/landing-page.compone
     StoreModule.forRoot({}, {}),
     EffectsModule.forRoot([]),
     StoreDevtoolsModule.instrument({maxAge: 25, logOnly: !isDevMode()}),
-    LandingPageComponent
+    AvatarModule
   ],
   providers: [],
+  exports: [
+    TopNavigationComponent
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/landing-page/landing-page.component.ts
+++ src/app/components/landing-page/landing-page.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
-  standalone: true,
   styleUrl: './landing-page.component.scss'
 })
 export default class LandingPageComponent {
