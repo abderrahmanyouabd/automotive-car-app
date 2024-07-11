a11a2c0c291a3cfacb6dfabab7aa1d486ee35f90
Abderrahman Youabd <youabd50@gmail.com>
2024-07-11 10:31:40 +0200

    fix: sort functionality by price

--- src/app/components/landing-page/car-list/car-list.component.ts
+++ src/app/components/landing-page/car-list/car-list.component.ts
@@ -1,30 +1,29 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {Observable, of} from "rxjs";
-import {SelectItem} from "primeng/api";
-import {Cars} from "../../../commonTypes";
-
+import { Component, Input, OnInit } from '@angular/core';
+import { BehaviorSubject, combineLatest, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { SelectItem } from 'primeng/api';
+import { Car } from '../../../commonTypes';
 
 @Component({
   selector: 'app-car-list',
   templateUrl: './car-list.component.html',
-  styleUrl: './car-list.component.scss'
+  styleUrls: ['./car-list.component.scss']
 })
-export default class CarListComponent implements OnInit{
+export default class CarListComponent implements OnInit {
   sortKey: any;
 
-  @Input()
-  public automotives$!: Observable<Cars>;
+  @Input() public automotives$!: Observable<Car[]>;
 
-  @Input()
-  public title: string | undefined;
-
+  @Input() public title: string | undefined;
 
   sortOptions!: SelectItem[];
-
   sortOrder!: number;
+  sortField!: keyof Car;
 
-  sortField!: string;
+  private sortFieldSubject = new BehaviorSubject<keyof Car>('price');
+  private sortOrderSubject = new BehaviorSubject<number>(1);
 
+  sortedAutomotives$!: Observable<Car[]>;
 
 
   ngOnInit() {
@@ -32,18 +31,43 @@ export default class CarListComponent implements OnInit{
       { label: 'Price High to Low', value: '!price' },
       { label: 'Price Low to High', value: 'price' }
     ];
+    this.sortedAutomotives$ = this.automotives$;
+
   }
 
   onSortChange(event: any) {
-    let value = event.value;
+
+    this.sortedAutomotives$ = combineLatest([
+      this.automotives$,
+      this.sortFieldSubject,
+      this.sortOrderSubject
+    ]).pipe(
+      map(([automotives, sortField, sortOrder]) => {
+        return automotives.slice().sort((a, b) => {
+          let result = 0;
+          // @ts-ignore
+          if (a[sortField] < b[sortField]) {
+            result = -1;
+            // @ts-ignore
+          } else if (a[sortField] > b[sortField]) {
+            result = 1;
+          }
+          return result * sortOrder;
+        });
+      })
+    );
+
+    const value: string = event.value;
 
     if (value.indexOf('!') === 0) {
       this.sortOrder = -1;
-      this.sortField = value.substring(1, value.length);
+      this.sortField = value.substring(1) as keyof Car;
     } else {
       this.sortOrder = 1;
-      this.sortField = value;
+      this.sortField = value as keyof Car;
     }
-  }
 
+    this.sortFieldSubject.next(this.sortField);
+    this.sortOrderSubject.next(this.sortOrder);
+  }
 }
